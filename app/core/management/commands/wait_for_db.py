9230f443bfb304d522e4a296ead3e8a430695a3f
smtamay <stamay@ixmatix.com>
2024-10-07 13:12:33 -0600

    ::white_check_mark: fix: add blank lines, remove whitespace, and ensure newline at end of file

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,14 +2,14 @@
 Django command to wait for database to be available
 """
 import time
-
 from psycopg2 import OperationalError as Psycopg2Error
-
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to await for database."""
+
     def handle(self, *args, **options):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
--- app/core/test/test_commands.py
+++ app/core/test/test_commands.py
@@ -1,13 +1,14 @@
 from unittest.mock import patch
-
 from psycopg2 import OperationalError as Psycopg2Error
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
-@patch('core.management.commands.wait_for_db.Command.check')  # Mock the check function
+
+# Mock the check function
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandsTestCase(SimpleTestCase):
+
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database is database ready"""
         patched_check.return_value = True
@@ -15,7 +16,7 @@ class CommandsTestCase(SimpleTestCase):
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')  # Mock the time.sleep function
-    def test_wait_for_db_delay(self,patched_sleep, patched_check):
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError"""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
                                     [OperationalError] * 3 + [True]
