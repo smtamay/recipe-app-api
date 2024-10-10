5dacf219ce9ebcaca2665f826cddf15a995802fa
smtamay <stamay@ixmatix.com>
2024-10-10 13:07:36 -0600

    :sparkles: add API route for user endpoints in urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,7 +18,7 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +27,5 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
     ),
+    path('api/user/', include('user.urls')),
 ]
